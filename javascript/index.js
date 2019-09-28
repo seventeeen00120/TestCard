7afe885daf85dbf420dcf6082ba93b250000a856
seventeeen00120 <55825263+seventeeen00120@users.noreply.github.com>
2019-09-28 16:52:56 +0800

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+function changeh1(){
+    document.getElementById("title1").className ="c2";
+}
+
+function changeImg(){
+    document.getElementById("pic1").innerHTML="<img src='../images/photo-1086584.jpeg'>";
+    document.getElementById("pic1").className ="Img1";
+}
+
+function changeLastPage()
+{
+  location.href="./page2.html" ;
+}
